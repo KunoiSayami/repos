b8280283811c75f49ebe3a1b7383a7e94cb6a979
KunoiSayami <i@leanhe.dev>
2021-10-22 02:02:18 +0800

    feat(script): Support auto sign and add new package
    Signed-off-by: KunoiSayami <i@leanhe.dev>

--- build_all.sh
+++ build_all.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 PKGDEST="${PWD}/repo/packages"
 SRCDEST="${PWD}/build"
+REPO_DEST="${PWD}/repo/kunoisayami.db.tar.xz"
+REPO_PENDING="${PWD}/repo/PENDING"
+touch "$REPO_PENDING"
 pushd kunoisayami
 for folder in */ ; do
     PACKAGE_NAME=$(printf $folder | sed 's/.$//')
     pushd "$folder"
-    pervpkg=$(ls "../../repo/packages/$PACKAGE_NAME"*)
     SRCPKGDEST=$SRCDEST SRCDEST=$SRCDEST PKGDEST=$PKGDEST makepkg --clean
-    if [ $? != 13 ] && [ -z "$pervpkg" ]; then
-        rm $pervpkg
+    if [ $? == 0 ]; then
+        echo "$PACKAGE_NAME" >> "$REPO_PENDING"
     fi
     #cp "$PACKAGE_NAME"* ../../../packages/
     popd
 done
 popd
+while read PACKAGE_NAME; do
+    LATEST=$(ls -t "$PKGDEST/$PACKAGE_NAME"* | head -n 1 | cut -d' ' -f1)
+    repo-add "$REPO_DEST" "$LATEST" -s -v -R
+done < "$REPO_PENDING"
 unset PACKAGE_NAME
+unset PKGDEST
+unset SRCDEST
+unset REPO_DEST
+rm -rf "$REPO_PENDING"
+unset REPO_PENDING
