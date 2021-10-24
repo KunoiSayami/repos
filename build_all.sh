02a313e149d6cda816a88bc7f9842f9d6b0a0d6c
KunoiSayami <i@leanhe.dev>
2021-10-24 12:18:00 +0800

    refactor: Add arch directory for packages
    Signed-off-by: KunoiSayami <i@leanhe.dev>

--- build_all.sh
+++ build_all.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-PKGDEST="${PWD}/packages"
+ARCH=$(uname -m)
+PKGDEST="${PWD}/packages/$ARCH"
 SRCDEST="${PWD}/build"
-REPO_DEST="${PWD}/packages/kunoisayami.db.tar.xz"
-REPO_PENDING="${PWD}/packages/PENDING"
+REPO_DEST="${PWD}/packages/$ARCH/kunoisayami.db.tar.xz"
+REPO_PENDING="${PWD}/packages/$ARCH/PENDING"
 touch "$REPO_PENDING"
 pushd kunoisayami
 for folder in */ ; do
     PACKAGE_NAME=$(printf $folder | sed 's/.$//')
     pushd "$folder"
-    SRCPKGDEST=$SRCDEST SRCDEST=$SRCDEST PKGDEST=$PKGDEST makepkg --clean --sign
+    SRCPKGDEST=$SRCDEST SRCDEST=$SRCDEST PKGDEST=$PKGDEST makepkg --clean --sign --key 4A0F0C8BC709ACA4341767FB243975C8DB9656B9
     if [ $? == 0 ]; then
         echo "$PACKAGE_NAME" >> "$REPO_PENDING"
     fi
@@ -20,6 +21,7 @@ while read PACKAGE_NAME; do
     LATEST=$(ls -t "$PKGDEST/$PACKAGE_NAME"* | grep -v ".sig" | head -n 1 | cut -d' ' -f1)
     repo-add "$REPO_DEST" "$LATEST" -s -v -R
 done < "$REPO_PENDING"
+echo $(date +%s) > "$PKGDEST/LASTBUILD"
 unset PACKAGE_NAME
 unset PKGDEST
 unset SRCDEST
