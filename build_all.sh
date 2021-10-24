c2c330dd9689883a08a61d4981f59bb1cee57db2
Coelacanthus <CoelacanthusHex@gmail.com>
2021-10-24 17:29:45 +0800

    refactor: using specified makepkg.conf to obtain a fixed compilation configuration
    Signed-off-by: Coelacanthus <CoelacanthusHex@gmail.com>

--- build_all.sh
+++ build_all.sh
@@ -2,6 +2,7 @@
 ARCH=$(uname -m)
 PKGDEST="${PWD}/packages/$ARCH"
 SRCDEST="${PWD}/build"
+CONFDEST="${PWD}/makepkg.conf"
 REPO_DEST="${PWD}/packages/$ARCH/kunoisayami.db.tar.xz"
 REPO_PENDING="${PWD}/packages/$ARCH/PENDING"
 touch "$REPO_PENDING"
@@ -9,7 +10,7 @@ pushd kunoisayami
 for folder in */ ; do
     PACKAGE_NAME=$(printf $folder | sed 's/.$//')
     pushd "$folder"
-    SRCPKGDEST=$SRCDEST SRCDEST=$SRCDEST PKGDEST=$PKGDEST makepkg --clean --sign --key 4A0F0C8BC709ACA4341767FB243975C8DB9656B9
+    SRCPKGDEST=$SRCDEST SRCDEST=$SRCDEST PKGDEST=$PKGDEST MAKEPKG_CONF=$CONFDEST makepkg --clean --sign --key 4A0F0C8BC709ACA4341767FB243975C8DB9656B9
     if [ $? == 0 ]; then
         echo "$PACKAGE_NAME" >> "$REPO_PENDING"
     fi
@@ -25,6 +26,7 @@ echo $(date +%s) > "$PKGDEST/LASTBUILD"
 unset PACKAGE_NAME
 unset PKGDEST
 unset SRCDEST
+unset CONFDEST
 unset REPO_DEST
 rm -rf "$REPO_PENDING"
 unset REPO_PENDING
