85730f64df10be1bdc66aa110651c84fbbded40f
Coelacanthus <CoelacanthusHex@gmail.com>
2021-10-25 20:45:54 +0800

    fix: print info of uploading package
    Signed-off-by: Coelacanthus <CoelacanthusHex@gmail.com>

--- ci-upload.sh
+++ ci-upload.sh
@@ -19,7 +19,9 @@ curl -d "token=$UPLOAD_TOKEN&action=REQUIRE_CLEAN" -fsSL "$REMOTE_PATH"
 pushd $PKGDEST
 
 for file in *.pkg*; do
+	echo "Start upload package $file ..."
     curl -F "file=@$file" -F "token=$UPLOAD_TOKEN" -fsSL "$REMOTE_PATH"
+	echo "Stop upload package $file"
 done
 
 popd
