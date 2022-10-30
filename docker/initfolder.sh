7ec83b0a1f6c26b142c6b2d08f26e0c9e38af6b7
KunoiSayami <i@leanhe.dev>
2022-10-31 03:17:19 +0800

    fix(script): Fix empty directory checker
    Signed-off-by: KunoiSayami <i@leanhe.dev>

--- docker/initfolder.sh
+++ docker/initfolder.sh
@@ -2,8 +2,9 @@
 
 if [ -f /home/build/repos/.placehold ]; then
   rm /home/build/repos/.placehold
-  find /home/build/repos -maxdepth 0 -empty -exec rm -rf {} \;
 fi
+
+find /home/build/repos -maxdepth 0 -empty -exec rm -rf {} \;
 
 if [ -d /home/build/repos ]; then
   chown -R build:build /home/build/repos
